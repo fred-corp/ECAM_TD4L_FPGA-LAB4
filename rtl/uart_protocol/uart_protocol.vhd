2bb1ce19d8441a006091c7b21d3ef68bcb77b527
fred-corp <frederic.druppel@icloud.com>
2025-03-24 12:10:34 +0100

    implemented APB Write logic

--- rtl/uart_protocol/uart_protocol.vhd
+++ rtl/uart_protocol/uart_protocol.vhd
@@ -33,8 +33,94 @@ entity uart_protocol is
 end entity uart_protocol;
 
 architecture rtl of uart_protocol is
+    type UART_STATUS is
+        (
+            WRITE_ADDRESS, WRITE_DATA0, WRITE_DATA1, WRITE_APB_SETUP, WRITE_APB_EXECUTE, WRITE_APB_DONE, WRITE_ANSWER_HEADER, WRITE_ANSWER_OK,
+            READ_ADDRESS, READ_APB_SETUP, READ_APB_EXECUTE, READ_APB_DONE, READ_ANSWER_HEADER, READ_ANSWER_DATA0, READ_ANSWER_DATA1,
+            IDLE
+        );
+    
+    signal state : UART_STATUS := IDLE;
+    signal write : std_logic := '0';
+    signal apb_data : std_logic_vector(15 downto 0);
+    signal address : std_logic_vector(7 downto 0);
+    signal wr_data : std_logic_vector(15 downto 0);
 
 begin
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                state <= IDLE;
+            end if;
 
+            case state is
+                when IDLE =>
+                    if rx_valid = '1' then
+                        if rx_data = x"AA" then
+                            state <= WRITE_ADDRESS;
+                            write <= '1';
+                        elsif rx_data = x"55" then
+                            state <= READ_ADDRESS;
+                            write <= '0';
+                        else
+                            state <= IDLE;
+                        end if;
+                    else
+                        state <= IDLE;
+                    end if;
+                when WRITE_ADDRESS =>
+                    if rx_valid = '1' then 
+                        address <= rx_data;
+                        state <= WRITE_DATA0;
+                    end if;
+                when WRITE_DATA0 =>
+                    if rx_valid = '1' then 
+                        wr_data(15 downto 8) <= rx_data;
+                        state <= WRITE_DATA1;
+                    end if;
+                when WRITE_DATA1 =>
+                    if rx_valid = '1' then 
+                        wr_data(7 downto 0) <= rx_data;
+                        state <= APB_SETUP;
+                    end if;
+                when APB_SETUP =>
+                    m_psel <= '1';
+                    m_paddr <= address;
+                    m_pwrite <= write;
+                    m_pwdata <= wr_data;
+                    m_penable <= '0';
+                    state <= APB_EXECUTE;
+                when APB_EXECUTE =>
+                    m_penable <= '1';
+                    apb_data <= m_prdata;
+                    state <= APB_DONE;
+                when APB_DONE =>
+                    m_psel <= '0';
+                    if write = '1' then
+                        state <= WRITE_ANSWER_HEADER;
+                    else
+                        state <= READ_ANSWER_HEADER;
+                    end if;
+                when WRITE_ANSWER_HEADER =>
+                    tx_data <= x"AA";
+                    tx_valid <= '1';
+                    if tx_ready = '1' then
+                        tx_valid <= '0';
+                        state <= WRITE_ANSWER_OK;
+                    end if;
+                when WRITE_ANSWER_OK =>
+                    tx_data <= x"00";
+                    tx_valid <= '1';
+                    if tx_ready = '1' then
+                        tx_valid <= '0';
+                        state <= IDLE;
+                    end if;
+                
+                when others =>
+                    null;
+            end case;
 
-end architecture;
+        end if;
+    end process;
+end rtl;
